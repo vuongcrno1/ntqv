16dc2b7b3b85eefa72a4ed2b6b8ff91a35735d1a
Vuong Nguyen <39398520+vuongcrno1@users.noreply.github.com>
2018-09-24 10:44:12 +0700

    Update README.java

--- README.java
+++ README.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World I am Nguyen Tran Quoc Vuong");
+        System.out.println("Hello World I am TDTU");
     }
 
 }
